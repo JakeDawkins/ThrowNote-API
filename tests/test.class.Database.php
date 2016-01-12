6f95b9e86ba0abed9b1c2ccfacaeb89aec3e6634
jakedawkins <dawkinsjh@gmail.com>
2016-01-12 11:28:57 -0500

    added setUp to phpunit test

--- tests/test.class.Database.php
+++ tests/test.class.Database.php
@@ -4,32 +4,28 @@ include(Path::models() . 'config.php');
 
 class DatabaseTest extends PHPUnit_Framework_TestCase {
 
+	//database object to be used by all tests
+	private $db;
+
 	public function setUp(){
-		$db = new Database();
+		$this->db = new Database();
 	}
-	public function tearDown(){}
+	public function tearDown(){
+		unset($this->db);
+	}
 
 	//------------------------ TESTS ------------------------
 
-	public function testDatabaseInit(){
-		$db = new Database();
-		$this->assertTrue($db != null);
-	}
-
 	public function testConnectSuccess(){
-		$db = new Database();
-
-		$mysqli = $db->connect();
+		$mysqli = $this->db->connect();
 		$this->assertTrue($mysqli != false);
 	}
 
 	public function testPrepareQuery(){
-		$db = new Database();
-
-		$sql = $db->prepareQuery("SELECT * FROM `table` WHERE `field1` = ? AND `field2` = ? AND ? AND `field3` = ?","lol 'WUT'", 13, true, null);
+		$sql = $this->db->prepareQuery("SELECT * FROM `table` WHERE `field1` = ? AND `field2` = ? AND ? AND `field3` = ?","lol 'WUT'", 13, true, null);
 		$this->assertEquals($sql, "SELECT * FROM `table` WHERE `field1` = 'lol \'WUT\'' AND `field2` = 13 AND TRUE AND `field3` = NULL");
 
-		$sql = $db->prepareQuery('UPDATE `some_table` SET `some_column` = ?, `some_other_column` = ?, `some_id` = ? WHERE `item` = ?', '20', 21, 69, 'this_val');
+		$sql = $this->db->prepareQuery('UPDATE `some_table` SET `some_column` = ?, `some_other_column` = ?, `some_id` = ? WHERE `item` = ?', '20', 21, 69, 'this_val');
 		$this->assertEquals($sql, "UPDATE `some_table` SET `some_column` = '20', `some_other_column` = 21, `some_id` = 69 WHERE `item` = 'this_val'");
 	}
 
@@ -37,10 +33,8 @@ class DatabaseTest extends PHPUnit_Framework_TestCase {
 	*	@prereq: must be records in test_notes
 	*/
 	public function testQuery(){
-		$db = new Database();
-
 		$sql = "SELECT * FROM test_notes";
-		$result = $db->query($sql)->num_rows;
+		$result = $this->db->query($sql)->num_rows;
 		$this->assertTrue($result > 0); 
 	}
 
@@ -50,14 +44,12 @@ class DatabaseTest extends PHPUnit_Framework_TestCase {
 	*		recond record in table has created timedate of "2016-01-10 17:10:10"
 	*/
 	public function testSelect(){
-		$db = new Database();
-
 		$sql = "SELECT text FROM test_notes WHERE id=1";
-		$result = $db->select($sql);
+		$result = $this->db->select($sql);
 		$this->assertEquals("test",$result[0]['text']);
 
 		$sql = "SELECT * FROM test_notes";
-		$result = $db->select($sql);
+		$result = $this->db->select($sql);
 		$this->assertTrue(count($result) > 0);
 		$this->assertEquals("2016-01-10 17:10:10", $result[1]['created']);
 	}
