0941761eb4c1246069f2f5824e248bd62a1140e6
Jake Dawkins <dawkinsjh@gmail.com>
2016-03-29 15:17:00 -0400

    git remote pull request

--- public_html/api/v1/class.ThrowNoteAPI.php
+++ public_html/api/v1/class.ThrowNoteAPI.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once 'class.API.php';
 require_once dirname(dirname(dirname(__FILE__))) . '/models/config.php';
 require_once dirname(dirname(dirname(__FILE__))) . '/models/config-uc.php';
